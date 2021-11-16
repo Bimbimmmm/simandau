eb8545162316499d0f937c5c5406e50bd9d1ad18
Bimbimmmm <bimasatriayudha1398@gmail.com>
2021-11-16 09:17:50 +0800

    add quotes and SOP

--- resources/views/public/aboutus/index.blade.php
+++ resources/views/public/aboutus/index.blade.php
@@ -1,19 +1,31 @@
 @extends('layouts.guest')
 @section('content')
-<div class="min-h-screen bg-indigo-900 py-14">
-  <div class="md:flex md:justify-center md:space-x-8 md:px-14">
-    <div class="w-full md:w-1/4 ml-auto mr-auto px-4">
-      <img alt="..." class="max-w-full rounded-lg shadow-lg" src="{{ asset('images/kacab.png') }}">
+<div class="min-w-screen min-h-screen bg-gray-200  items-center justify-center px-5 py-5">
+  <div class="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style="max-width: 1500px  ">
+        <div class="w-full pt-1 pb-5">
+            <div class="overflow-hidden rounded-full w-48 -mt-16 mx-auto shadow-lg">
+                <img src="{{ asset('images/kacab.png') }}" alt="">
+            </div>
+        </div>
+        <div class="w-full mb-10">
+            <p class="text-sm text-gray-600 text-center px-5 mt-2">
+              Penguatan pendidikan menjadi kebutuhan yang mendesak mengingat semakin menguatnya proses degradasi moral generasi muda bangsa ini. Pendidikan dan sistemnya yang selama ini dilaksanakan di Indonesia, memunculkan banyak  pertanyaan yang menggambarkan kegelisahan tentang bagaimana pendidikan berperan dengan sebenarnya. Pendidikan selama ini diharapkan sebagai <i>agent of change</i> dalam upaya pembentukan dan penanaman nilai-nilai budaya  menjadi perilaku dan proses budaya.
+            </p>
+            <p class="text-sm text-gray-600 text-center px-5 mt-2">
+              Seyogyanya, melalui pendidikan, bangsa dapat mengembangkan sikap dan kebudayaannya sendiri agar generasi muda bangsa ini tidak terperangkap dalam kontak kebudayaan dengan budaya asing yang memiliki nilai dan standar norma yang berbeda atau bertentangan sehingga mereka mengalami gegar budaya <i>(shock culture)</i>. Dalam konteks pembangunan karakter bangsa, penetrasi dan pelestarian kebudayaan dapat dimasifkan dengan pendidikan karena pendidikan sendiri tidak bisa juga melepaskan diri dari ruang lingkup kebudayaan, kebudayaan juga merupakan hasil dari proses interaksi kebudayaan manusia sebagai hasil proses pendidikan
+            </p>
+            <p class="text-sm text-gray-600 text-center px-5 mt-2">
+              Kebudayaan dan kearifan lokal diwariskan atau diturunkan kepada generasi demi generasi lewat proses pembelajaran  baik formal maupun non formal, terlembaga atau dalam keluarga sehingga substansi kebudayaan itu melekat dalam pribadi generasi bangsa sebagai hasil dari proses pendidikan. Pendidikan harus menjadi dasar dari kebudayaan dan kebudayaan juga menjadi dasar dari pendidikan. Pengokohan pendidikan dan kebudayaan bukan hanya cita cita tapi yang lebih penting adalah pelaksanaan dari dasar-dasarnya sehingga tujuan dan fungsi penguatan dan pengokohan pendidikan dan kebudayaan dapat tercapai dengan baik, seterusnya dapat mewarnai karakter bangsa ini secara berkesinambungan.
+            </p>
+            <p class="text-sm text-gray-600 text-center px-5 mt-2">
+              Salah satu potensi yang dapat dimanfaatkan untuk menguatkan pendidikan dan pewarisan kebudayaan adalah teknologi informasi yang mampu melintasi ruang dan waktu dan dapat menyebarkan informasi berupa ilmu pengetahuan dan nilai nilai kebudayaan dengan jangkauan yang melintasi batas teritori dan sekat-sekat wilayah, keragaman suku, agama, ras, maupun mereduksi kepentingan-kepentingan politis dan hampir menghapuskan perbedaan status/strata sosial ekonomi. Jika potensi ini terkelola dengan baik, kita bisa berharap banyak upaya penguatan pendidikan dan pewarisan kebudayaan dapat lebih mudah diraih.Semoga……
+            </p>
+        </div>
+        <div class="w-full">
+            <p class="text-xl text-indigo-500 font-bold text-center">Dr. Nurbaya, S.Si, MP.</p>
+            <p class="text-md text-gray-500 text-center">Kepala Cabang Dinas</p>
+        </div>
     </div>
-    <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
-      <div class="md:pr-12">
-        <h3 class="text-2xl font-semibold text-white">"Kuatkan Pendidikan, Lestarikan Budaya!"</h3>
-        <p class="mt-4 text-lg leading-relaxed text-white">
-          
-        </p>
-      </div>
-    </div>
-  </div>
 </div>
 <div class="min-h-screen bg-white py-14">
   <h1 class="mb-12 text-center text-4xl text-black font-bold">Tugas Pokok dan Fungsi</h1>
@@ -216,13 +228,42 @@
       </div>
     </div>
   </div>
-
 </div>
 <div class="min-h-screen bg-blue-200 py-14">
+  <h1 class="mb-12 text-center text-4xl text-black font-bold">Standar Operasional Prosedur (SOP)</h1>
+  <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+    <div class=" px-10 py-6 mx-auto">
+      <div class="max-w-4xl px-10 py-6 mx-auto bg-gray-50">
+        <a class="block transition duration-200 ease-out transform hover:scale-110">
+          <img class="object-cover w-full shadow-sm h-full" src="{{ asset('images/services/maklumat-pelayanan.jpg') }}">
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+    <div class=" px-10 py-6 mx-auto">
+      <div class="max-w-4xl px-10 py-6 mx-auto bg-gray-50">
+        <a class="block transition duration-200 ease-out transform hover:scale-110">
+          <img class="object-cover w-full shadow-sm h-full" src="{{ asset('images/services/standar-pelayanan.jpg') }}">
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+    <div class=" px-10 py-6 mx-auto">
+      <div class="max-w-4xl px-10 py-6 mx-auto bg-gray-50">
+        <a class="block transition duration-200 ease-out transform hover:scale-110">
+          <img class="object-cover w-full shadow-sm h-full" src="{{ asset('images/services/alur-legalisir.jpg') }}">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="min-h-screen bg-green-200 py-14">
   <h1 class="mb-12 text-center text-4xl text-black font-bold">Struktur Organisasi</h1>
   <div class="md:flex md:justify-center md:space-x-8 md:px-14">
     <div class=" px-10 py-6 mx-auto">
-      <div class="max-w-6xl px-10 py-6 mx-auto bg-gray-50">
+      <div class="max-w-4xl px-10 py-6 mx-auto bg-gray-50">
         <a class="block transition duration-200 ease-out transform hover:scale-110">
           <img class="object-cover w-full shadow-sm h-full" src="{{ asset('images/struktur.jpg') }}">
         </a>
