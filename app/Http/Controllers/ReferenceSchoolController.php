f6504cc88f71022c5c9930c1e88a5fdec1186481
Bimbimmmm <bimasatriayudha1398@gmail.com>
2021-11-20 15:55:22 +0800

    bug fixed

--- app/Http/Controllers/ReferenceSchoolController.php
+++ app/Http/Controllers/ReferenceSchoolController.php
@@ -21,7 +21,7 @@ class ReferenceSchoolController extends Controller
      */
     public function index()
     {
-        $datas = ReferenceSchool::where('is_deleted' => FALSE)->get();
+        $datas = ReferenceSchool::where('is_deleted', FALSE)->get();
         return view('administrator/references/school/index', compact('datas'));
     }
 
